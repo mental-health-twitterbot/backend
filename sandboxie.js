89b4e57e51562e0eb3bba7770b1ec09f5eaa7fc4
Josh Ford <thisisjoshford@gmail.com>
2020-04-03 17:49:50 -0700

    sandbox scraper

--- sandboxie.js
+++ sandboxie.js
@@ -5,5 +5,6 @@ fetch('https://www.nami.org/Learn-More/Mental-Health-By-the-Numbers')
   .then(res => res.text())
   .then(text => html.parse(text))
   .then(document => {
-    console.log(document.querySelector('li'));
+    console.log(document.querySelectorAll('.content li').map(content => content.text)[4]);
   });
+//html.querySelectorAll('.pi-data-label').map(label => label.structuredText);
