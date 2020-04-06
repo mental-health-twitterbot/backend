e4e88fb22afc091107dc6cf83a54b212510acde7
janelle mellor <janellemellor@gmail.com>
2020-04-06 16:44:21 -0700

    remove question type from Fact model

---
+++ __tests__/facts.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../lib/app');
+const { getFact, getFacts } = require('../db/db-helpers');
+
+describe('fact routes', () => {
+  it('gets all facts', async() => {
+  
+  })  
+});
+
+
+//FACT ROUTES
+
+//GET routes for facts
+
+
