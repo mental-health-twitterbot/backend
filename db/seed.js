ddb5fc7a7703988abb43a10492949df552d5f95f
janelle mellor <janellemellor@gmail.com>
2020-04-06 11:42:24 -0700

    add facts to seed data

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,7 @@
 const chance = require('chance').Chance();
+const Fact = require('../lib/models/Fact');
 
-module.exports = async({ exampleToCreate = 10 } = {}) => {
+module.exports = async({ exampleToCreate = 10, factsToCreate = 10 } = {}) => {
 
   const example = await Example.create([...Array(exampleToCreate)].map(() => ({
     name: `${chance.animal()} Studios`, 
@@ -9,6 +10,19 @@ module.exports = async({ exampleToCreate = 10 } = {}) => {
       state: chance.state(),
       country: chance.pickone(country)
     }
+  })));
+
+  const sources = ['NAMI', 'National Institute of Mental Health', 'Brain & Behavior Research Foundation', 'Trans Lifeline'];
+
+  const source_urls = ['nami.org', 'nimh.nih.gov', 'bbrfoundation.org', 'translifeline.org']
+
+  const facts = await Fact.create([...Array(factsToCreate)].map(() => ({
+    text: chance.sentence(),
+    source: chance.pickone(sources),
+    source_url: chance.pickone(source_urls),
+    site_timestamp: chance.timestamp(),
+    type: chance.pickone(questions)._id,
+    has_tweeted: chance.bool()
   })));
 
 };
