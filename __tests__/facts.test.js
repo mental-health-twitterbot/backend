4153efba432ea6ccc54db9d3fd6c57bad5be41ce
janelle mellor <janellemellor@gmail.com>
2020-04-06 16:53:36 -0700

    add test for get all facts route

--- __tests__/facts.test.js
+++ __tests__/facts.test.js
@@ -4,13 +4,22 @@ const { getFact, getFacts } = require('../db/db-helpers');
 
 describe('fact routes', () => {
   it('gets all facts', async() => {
-  
-  })  
-});
+    const facts = await getFacts();
+
+    return request(app)
+      .get('/api/v1/facts')
+      .then(res => {
+        expect(res.body).toEqual(facts);
+      });
+  });
+
+}); 
+
 
 
 //FACT ROUTES
 
-//GET routes for facts
+//GET ALL facts
+//GET Facts by hashtag type
 
 
