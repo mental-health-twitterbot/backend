e064e473d45e64c47143f5a606863c2b28412bb4
janelle mellor <janellemellor@gmail.com>
2020-04-07 15:54:20 -0700

    update get route for Tweet

--- __tests__/tweets.js
+++ __tests__/tweets.js
@@ -5,7 +5,7 @@ const { getTweet } = require('../db/db-helpers');
 describe('tweet routes', () => { 
     
   //GET route from our db
-  it('geta a tweet', async() => {
+  it('gets a tweet', async() => {
     const tweet = await getTweet();
 
     return request(app)
