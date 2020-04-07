28f6bd0cac8059c94d61671b793b746cfc43799f
Josh Ford <thisisjoshford@gmail.com>
2020-04-06 20:14:01 -0700

    remove question type

--- __tests__/question.test.js
+++ __tests__/question.test.js
@@ -1,4 +1,4 @@
-const { getActor, getActors } = require('../db/data-helpers');
+const { getQuestion } = require('../db/db-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -8,24 +8,14 @@ describe('app routes', () => {
 
   it('gets a question', () => {
     return request(app)
-      .post('/api/v1/actor')
-      .send({
-        name: 'Mr. Bean',
-        dob: '02/19/1982',
-        pob: 'Milwaukee, WI'
-      })
+      .get('/api/v1/question')
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          name: 'Mr. Bean',
-          dob: expect.any(String),
-          pob: 'Milwaukee, WI',
+          question_text: expect.any(String),
           __v: 0
         });
       });
   });
-
-  
-
 });
 
