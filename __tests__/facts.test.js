4acf86cfc697deb6c8d08ff97b31c8db0e5725bb
janelle mellor <janellemellor@gmail.com>
2020-04-06 19:26:15 -0700

    fix test route for get facts by hashtag type

--- __tests__/facts.test.js
+++ __tests__/facts.test.js
@@ -14,13 +14,13 @@ describe('fact routes', () => {
   });
 
   it('gets a fact by hashtag type', async() => {
-    const fact = await getFact();
     
     return request(app)
-      .get(`/api/v1/facts/${fact.hashtag_type}`)
+      .get('/api/v1/facts/types')
       .then(res => {
-        expect(res.body).toEqual({
-          ...fact
+        expect(res.body).toContainEqual({
+          _id: expect.any(String),
+          facts: expect.any(Array) 
         });
       });
   });
