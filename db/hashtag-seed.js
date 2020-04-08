09bde9898d986be9eb7aad1f578490f1cfe6ca76
janelle mellor <janellemellor@gmail.com>
2020-04-08 08:52:42 -0700

    add sample hashtag seed data

--- db/hashtag-seed.js
+++ db/hashtag-seed.js
@@ -1,5 +1,66 @@
 const hashtagData = [
+  {
+    id: 1, 
+    hashtag: 'MentalHealthMatters',
+    type: 'general'
+  },
+  {
+    id: 2, 
+    hashtag: 'MentalHealthAwareness',
+    type: 'general' 
+  },
+  {
+    id: 3, 
+    hashtag: 'MentalHealth',
+    type: 'general'
+  },
+  {
+    id: 4, 
+    hashtag: 'StopTheStigma',
+    type: 'general'
+  },
+  {
+    id: 5, 
+    hashtag: 'depression',
+    type: 'keyword' 
+  },
+  {
+    id: 6, 
+    hashtag: 'anxiety',
+    type: 'keyword' 
+  },
+  {
+    id: 7, 
+    hashtag: 'schizophrenia',
+    type: 'keyword' 
+  },
+  {
+    id: 8, 
+    hashtag: 'adhd',
+    type: 'keyword' 
+  },
+  {
+    id: 9, 
+    hashtag: 'bipolar',
+    type: 'keyword' 
+  },
+  {
+    id: 10, 
+    hashtag: 'autism',
+    type: 'keyword' 
+  },
+  {
+    id: 11, 
+    hashtag: 'ocd',
+    type: 'keyword' 
+  },
 
 ];
 
 module.exports = hashtagData;
+
+
+//Use either 'general' or 'keyword' for type
+
+
+
