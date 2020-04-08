2354a428f83f516faaa8bb504e857f4cb898801d
janelle mellor <janellemellor@gmail.com>
2020-04-08 08:09:41 -0700

    add sample questions and structure to seed data

---
+++ db/hashtag-seed.js
@@ -0,0 +1,5 @@
+const hashtagData = [
+
+];
+
+module.exports = hashtagData;
---
+++ db/question-seed.js
@@ -0,0 +1,44 @@
+const questionData = [
+  {
+    id: 1, 
+    question: 'What is your experience with this?'
+  },
+  {
+    id: 2, 
+    question: 'What do you think?',
+  },
+  {
+    id: 3, 
+    question: 'Can you relate?'
+  },
+  {
+    id: 4,
+    question: 'How do you feel about this?'
+  },
+  {
+    id: 5,
+    question: 'Do you have a similar story?'
+  },
+  {
+    id: 6,
+    question: 'Do you or someone you know have experience with this?'
+  },
+  {
+    id: 7,
+    question: 'What do you think we can do to change this?'
+  },
+  {
+    id: 8,
+    question: 'How can we do things differently?',
+  },
+  {
+    id: 9, 
+    question: 'What would you like to see change?'
+  },
+
+
+];
+
+module.exports = questionData;
+
+
