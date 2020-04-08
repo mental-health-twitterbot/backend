527639d0961181aed7de42c3812a23f12c18fd28
thisisjoshford <thisisjoshford@gmail.com>
2020-04-07 17:21:38 -0700

    Datacleanup (#36)
    * split by line break
    
    * removed returns and /t
    
    * cleanup strings
    
    * nami text fix
    
    * refactored
    
    * refactored
    
    * nimh clean
    
    * added some data cleaning (#35)
    
    Co-authored-by: Dakota Druley <58957027+dakotadruley@users.noreply.github.com>

--- sandboxie.js
+++ sandboxie.js
@@ -1,19 +1,55 @@
-
-const { who, whoMentalDisorder, whoSchizophrenia } = require('./lib/scrappers/who');
-const { anxietyDisorder } = require('./lib/scrappers/ADAA');
+const { nimh } = require('./lib/scrappers/nimh');
 const { anxietyFacts } = require('./lib/scrappers/promisesbehavioral');
-who()
-  .then(data => console.log(data));
 
-whoMentalDisorder()
-  .then(data => console.log(data));
+// const { who, whoMentalDisorder, whoSchizophrenia } = require('./lib/scrappers/who');
+// const { anxietyDisorder } = require('./lib/scrappers/ADAA');
+// const { anxietyFacts } = require('./lib/scrappers/promisesbehavioral');
+// // who()
+//   .then(data => console.log(data));
 
-whoSchizophrenia()
-  .then(data => console.log(data));
+// whoMentalDisorder()
+//   .then(data => console.log(data));
 
-anxietyDisorder()
-  .then(data => console.log(data));
+// whoSchizophrenia()
+//   .then(data => console.log(data));
 
-anxietyFacts()
-  .then(data => console.log(data));
 
+//DONE
+// eatingRecoveryCenter()
+//   .then(data => console.log(data));
+
+//DONE
+// anxietyDisorder()
+//   .then(data => console.log(data));
+
+//DONE
+// hopeForDepression()
+//   .then(data => console.log(data));
+
+//DONE
+// nami()
+//   .then(data => console.log(data));
+
+// nimh()
+//   .then(data => console.log(data));
+
+// DAKOTA STARTED HERE
+// DONE
+// who()
+//   .then(data => console.log(data));
+
+// DONE
+// whoMentalDisorder()
+//   .then(data => console.log(data));
+
+// DONE
+// whoSchizophrenia()
+//   .then(data => console.log(data));
+
+// DONE
+// self()
+//   .then(data => console.log(data));
+
+// DONE
+// anxietyFacts()
+//   .then(data => console.log(data));
