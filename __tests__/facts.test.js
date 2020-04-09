2d43c311e29b13fd272ea5092e5b30a27da36150
janelle mellor <janellemellor@gmail.com>
2020-04-09 15:27:58 -0700

    delete test and route for find fact by type

--- __tests__/facts.test.js
+++ __tests__/facts.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
-const { getFact, getFacts } = require('../db/db-helpers');
+const { getFacts } = require('../db/db-helpers');
 
 describe('fact routes', () => {
   it('gets all facts', async() => {
@@ -12,26 +12,4 @@ describe('fact routes', () => {
         expect(res.body).toEqual(facts);
       });
   });
-
-  it('gets a fact by hashtag type', async() => {
-    
-    return request(app)
-      .get('/api/v1/facts/types')
-      .then(res => {
-        expect(res.body).toContainEqual({
-          _id: expect.any(String),
-          facts: expect.any(Array) 
-        });
-      });
-  });
-
-}); 
-
-
-
-//FACT ROUTES
-
-//GET ALL facts
-//GET Facts by hashtag type
-
-
+});
