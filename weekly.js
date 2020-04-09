288dc4cca2b99e7d571812dad7cd960252b1ec0c
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-04-09 15:24:29 -0700

    added some adjustments to fix the mailgun

--- weekly.js
+++ weekly.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 const Tweet = require('./lib/models/Tweet');
-const sendSuggestedTweets = require('./lib/utils/mailgun');
+const { sendSuggestedTweets } = require('./lib/utils/mailgun');
 
 // Tweet.generate(); envoke the 10 tweet function = suggestedTweets .then
 // sendSuggestedTweets(suggestedTweets); envoke the email function
 // on click from the mailgun email, update the database tweets to 'approved'
 
-weekly();
-
 async function weekly() {
   const suggestedTweets = await Tweet.generate();
-  sendSuggestedTweets(suggestedTweets);
+  const mailResult = await sendSuggestedTweets(suggestedTweets);
+  console.log(mailResult);
 }
 
+weekly();
 // { tweet: ", tweet_url:"} this is where we will pass the approval get route
 // make a virtual with tweet_url
 
