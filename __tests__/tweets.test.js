fc8b9d90d5961532c56208924b191e0d7d957a16
janelle mellor <janellemellor@gmail.com>
2020-04-09 14:00:16 -0700

    update get test routes to use async await

--- __tests__/tweets.test.js
+++ __tests__/tweets.test.js
@@ -1,33 +1,37 @@
 const request = require('supertest');
 const app = require('../lib/app');
-const { getTweet } = require('../db/db-helpers');
+const { getTweet, getTweets } = require('../db/db-helpers');
 
 describe('tweet routes', () => { 
     
   //GET route from our db
-  it('gets a single tweet', () => {  
+  it('gets a single tweet', async() => {  
+    const tweet = await getTweet();
+
     return request(app)
       .get('/api/v1/tweets')
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          tweet_text: expect.any(String),
-          approved: 'false',
-          has_tweeted: 'false',
+          tweet_text: tweet.tweet_text,
+          approved: tweet.approved,
+          has_tweeted: tweet.has_tweeted,
           __v: 0
         });
       });
   });
 
-  it('gets all tweets', () => {
+  it('gets all tweets', async() => {
+    const tweets = await getTweets();
+
     return request(app)
       .get('/api/v1/tweets')
       .then(res => {
         expect(res.body).toContainEqual({
           _id: expect.any(String),
-          tweet_text: expect.any(String),
-          approved: 'false',
-          has_tweeted: 'false',
+          tweet_text: tweets.tweet_text,
+          approved: tweets.approved,
+          has_tweeted: tweets.has_tweeted,
           __v: 0
         });
       });
