46dcc4ccb9561c349bee88d685d10511d5441ac5
janelle mellor <janellemellor@gmail.com>
2020-04-09 13:36:16 -0700

    add failing get route for tweets

--- __tests__/tweets.test.js
+++ __tests__/tweets.test.js
@@ -5,15 +5,16 @@ const { getTweet } = require('../db/db-helpers');
 describe('tweet routes', () => { 
     
   //GET route from our db
-  it('gets a tweet', async() => {
-    const tweet = await getTweet();
-
-    //need to update 
+  it('gets a tweet', () => {
     return request(app)
       .get('/api/v1/tweets')
       .then(res => {
         expect(res.body).toEqual({
-          ...tweet
+          _id: expect.any(String),
+          tweet_text: expect.any(String),
+          approved: 'false',
+          has_tweeted: 'false',
+          __v: 0
         });
       });
   });
