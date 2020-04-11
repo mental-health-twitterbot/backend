928abd6c8116ecd69392cb755b0b15172a021714
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-11 15:15:01 -0700

    refactor

--- daily.js
+++ daily.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 const mongoose = require('mongoose');
 const Tweet = require('./lib/models/Tweet');
+const { sendEmail } = require('./lib/utils/mailgun');
 
 // post 'approved' tweets daily and not yet tweeted and tweet it 
 Tweet.findOneAndUpdate({
@@ -12,7 +13,21 @@ Tweet.findOneAndUpdate({
   has_tweeted: true
 }
 )
-  .then(tweet => console.log(`would Have Tweeted Tweet ${tweet}`))
+  .then(tweet => {
+    // send an email if there are no approved tweets to send
+    if(!tweet) {
+      return sendEmail({
+        subject: 'No tweets :(',
+        text: 'No approved tweets to send today'
+      });
+    }
+
+    // send an email when an approved tweet would have been tweeted
+    return sendEmail({
+      subject: 'Sending tweet!',
+      text: `We sent out a tweet: ${tweet.tweet_text}`
+    });
+  })
   .finally(() => mongoose.connection.close());
 
 //   once have twiiter integration instead of concole loging (sendTweet();)
